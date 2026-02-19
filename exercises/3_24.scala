cfa75ee5027c86dcfe3fa5d017458f2f7a47a9e1
Emeka Mbazor <embazor@gmail.com>
2026-02-19 15:50:15 -0800

    Improve solution for Exercise 3.24

--- exercises/3_24.scala
+++ exercises/3_24.scala
@@ -7,29 +7,22 @@ object List {
         if (as.isEmpty) Nil
         else Cons(as.head, apply(as.tail: _*))
 
+    def startsWith[A](sup: List[A], sub: List[A]): Boolean = {
+        (sup, sub) match {
+            case (Cons(aHead, aTail), Cons(bHead, bTail)) if aHead == bHead => startsWith(aTail, bTail)
+            case (Cons(aHead, aTail), Nil) => true
+            case _ => false
+        }
+    }
+
     def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {
-        def go1(a: List[A], b: List[A]): Boolean = {
-            def go2(c: List[A], d: List[A]): Boolean = {
-                (c, d) match {
-                    case (Cons(cHead, cTail), Cons(dHead, dTail)) if cHead == dHead => go2(cTail, dTail)
-                    case (Cons(cHead, cTail), Cons(dHead, dTail)) if cHead != dHead => false 
-                    case (Nil, Cons(dHead, dTail)) => false
-                    case (Cons(cHead, cTail), Nil) => true
-                    case (Nil, Nil) => true
-                }
-            }
-            (a, b) match {
-                case (Cons(aHead, aTail), Cons(bHead, bTail)) if aHead == bHead => if (go2(aTail, bTail)) true else go1(aTail, b)
-                case (Cons(aHead, aTail), Cons(bHead, bTail)) => go1(aTail, b)
-                case (Nil, Cons(bHead, bTail)) => false
-                case (Cons(aHead, aTail), Nil) => true
-                case (Nil, Nil) => false
-            }
+        (sup, sub) match {
+            case (Cons(aHead, aTail), Cons(bHead, bTail)) => startsWith(sup, sub) || hasSubsequence(aTail, sub)
+            case (Nil, Cons(bHead, bTail)) => false
+            case (Cons(aHead, aTail), Nil) => true
+            case (Nil, Nil) => true
         }
-        if (sup == Nil && sub == Nil) {
-            true
-        }
-        go1(sup, sub)
+
     }
 }
 
@@ -40,7 +33,8 @@ object Exercise_3_24 {
         println(List.hasSubsequence(List(1, 2, 3, 4), List(1, 2)))
         println(List.hasSubsequence(List(1, 2, 3, 4), List(2, 3)))
         println(List.hasSubsequence(List(1, 2, 3, 4), List(4)))
-
+       
+        println(List.hasSubsequence(List(1, 2, 3, 4), List(1, 3, 4)))
         println(List.hasSubsequence(List(1, 2, 3, 4), List(5)))
         println(List.hasSubsequence(List(1, 2, 3, 4), List(0, 1, 2, 3, 4)))
     }
